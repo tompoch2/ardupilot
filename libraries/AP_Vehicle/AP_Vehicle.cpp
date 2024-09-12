302a30b529552257b78448e73578cebc81b98b24
Andy Piper <github@andypiper.com>
2024-09-12 09:56:16 +0100

    AP_Vehicle: remove runcam singleton

--- libraries/AP_Vehicle/AP_Vehicle.cpp
+++ libraries/AP_Vehicle/AP_Vehicle.cpp
@@ -34,11 +34,6 @@ extern AP_IOMCU iomcu;
   2nd group of parameters
  */
 const AP_Param::GroupInfo AP_Vehicle::var_info[] = {
-#if HAL_RUNCAM_ENABLED
-    // @Group: CAM_RC_
-    // @Path: ../AP_Camera/AP_RunCam.cpp
-    AP_SUBGROUPINFO(runcam, "CAM_RC_", 1, AP_Vehicle, AP_RunCam),
-#endif
 
 #if HAL_GYROFFT_ENABLED
     // @Group: FFT_
@@ -449,9 +444,6 @@ void AP_Vehicle::setup()
     gyro_fft.init(1000);
 #endif
 #endif
-#if HAL_RUNCAM_ENABLED
-    runcam.init();
-#endif
 #if HAL_HOTT_TELEM_ENABLED
     hott_telem.init();
 #endif
@@ -613,9 +605,6 @@ const AP_Scheduler::Task AP_Vehicle::scheduler_tasks[] = {
     SCHED_TASK_CLASS(AP_Notify,    &vehicle.notify,         update,                   50, 300, 78),
 #if HAL_NMEA_OUTPUT_ENABLED
     SCHED_TASK_CLASS(AP_NMEA_Output, &vehicle.nmea,         update,                   50, 50, 180),
-#endif
-#if HAL_RUNCAM_ENABLED
-    SCHED_TASK_CLASS(AP_RunCam,    &vehicle.runcam,         update,                   50, 50, 200),
 #endif
 #if HAL_GYROFFT_ENABLED
     SCHED_TASK_CLASS(AP_GyroFFT,   &vehicle.gyro_fft,       update,                  400, 50, 205),
